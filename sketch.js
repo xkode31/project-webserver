24fe31ea1d1769eb892d3fc5d3167dd36fdc07d7
xkode31 <xkode31@gmail.com>
2017-01-30 15:01:21 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,3 @@
-/* - Relógio - */
-
-function getTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
-    var s=today.getSeconds();
-    // add a zero in front of numbers<10
-    m=checkTime(m);
-    s=checkTime(s);
-    document.getElementById('showtime').innerHTML=h+":"+m+":"+s;
-    t=setTimeout(function(){getTime()},500);
-}
-
-
 function checkTime(i)
 {
     if (i<10)
